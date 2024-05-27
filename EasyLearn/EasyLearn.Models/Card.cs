fed01f97a5b0bd0e5d24a9fd55997b756b4aad5c
vladyslav <dalbayb281@gmail.com>
2024-05-27 15:36:01 +0300

    Add EasyLearn.Models

---
+++ EasyLearn/EasyLearn.Models/Card.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyLearn.Models
+{
+    internal class Card
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Term { get; set; }
+        public string? Definition {  get; set; }
+        public byte[]? Image { get; set; }
+    }
+}
---
+++ EasyLearn/EasyLearn.Models/TrainingModule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyLearn.Models
+{
+    internal class TrainingModule
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public IEnumerable<Card> Cards { get; set; }
+        public DateTime Create {  get; set; }
+        public DateTime LastOpen { get; set; }
+        public bool IsLearned { get; set; } = false;
+    }
+}
