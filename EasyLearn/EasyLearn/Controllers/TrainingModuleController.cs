eacc196e42545e1466730e244ca9e3eac7f25f1d
MaksymKratasiuk <makshm1307@gmail.com>
2024-09-03 12:11:00 +0200

    Почата робота з локалізатором

--- EasyLearn/EasyLearn/Controllers/TrainingModuleController.cs
+++ EasyLearn/EasyLearn/Controllers/TrainingModuleController.cs
@@ -1,6 +1,4 @@
-﻿using EasyLearn.Data;
-using EasyLearn.Models;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Linq;
