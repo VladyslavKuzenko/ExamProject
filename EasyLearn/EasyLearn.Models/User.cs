80a5810fac13d8de38e5a66609d8811dd19e8778
MaksymKratasiuk <makshm1307@gmail.com>
2024-09-04 19:00:24 +0200

    Була почата робота з Identity, встановлені та оновлені дерективи, змінена модель user, але не остаточно

--- EasyLearn/EasyLearn.Models/User.cs
+++ EasyLearn/EasyLearn.Models/User.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +7,9 @@ using System.Threading.Tasks;
 
 namespace EasyLearn.Models
 {
-    public class User
+    public class User : IdentityUser
     {
-        public int Id { get; set; }
         public string Name { get; set; } = default!;
-        public string Email { get; set; } = default!;
         public string Login { get; set; } = default!;
         public string Password { get; set; } = default!;
         public byte[]? Avatar { get; set; }
--- EasyLearn/EasyLearn/Data/ApplicationDbContext.cs
+++ EasyLearn/EasyLearn/Data/ApplicationDbContext.cs
@@ -5,7 +5,7 @@ using EasyLearn.Models;
 
 namespace EasyLearn.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<User>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
