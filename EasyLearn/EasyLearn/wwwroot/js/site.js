3c87b9127220a5d6ce27f06ba2d06e4eb09d68a8
B1mba <partytopmaker@gmail.com>
2024-09-06 19:44:30 +0300

    Новий адаптивний дизайн, Home буде перероблений

--- EasyLearn/EasyLearn/wwwroot/js/site.js
+++ EasyLearn/EasyLearn/wwwroot/js/site.js
@@ -1,16 +1,42 @@
-﻿const divSet1 = document.getElementById('div-set-1');
-const divSet2 = document.getElementById('div-set-2');
-const prevBtn = document.getElementById('prev-btn');
-const nextBtn = document.getElementById('next-btn');
+﻿const menuToggle = document.getElementById('menu-toggle');
+const mobileMenu = document.getElementById('mobile-menu');
+const closeMenu = document.getElementById('close-menu');
 
-// Функція для перегортання на div-set-2
-nextBtn.addEventListener('click', function () {
-    divSet1.style.display = 'none';
-    divSet2.style.display = 'flex';
+menuToggle.addEventListener('click', function () {
+    mobileMenu.classList.toggle('open');
 });
 
-// Функція для перегортання на div-set-1
-prevBtn.addEventListener('click', function () {
-    divSet1.style.display = 'flex';
-    divSet2.style.display = 'none';
+closeMenu.addEventListener('click', function () {
+    mobileMenu.classList.remove('open');
+});
+
+// Close menu on larger screens
+window.addEventListener('resize', function () {
+    if (window.innerWidth > 768) {
+        mobileMenu.classList.remove('open');
+    }
+});
+
+
+let currentSet = 1;
+const maxSets = 2; // кількість наборів карток
+
+document.querySelector('.right-btn').addEventListener('click', () => {
+    document.querySelectorAll(`#set-${currentSet}`).forEach(card => {
+        card.style.display = 'none'; // Приховуємо попередні блоки
+    });
+    currentSet = currentSet === maxSets ? 1 : currentSet + 1;
+    document.querySelectorAll(`#set-${currentSet}`).forEach(card => {
+        card.style.display = 'flex'; // Відображаємо нові блоки
+    });
+});
+
+document.querySelector('.left-btn').addEventListener('click', () => {
+    document.querySelectorAll(`#set-${currentSet}`).forEach(card => {
+        card.style.display = 'none'; // Приховуємо попередні блоки
+    });
+    currentSet = currentSet === 1 ? maxSets : currentSet - 1;
+    document.querySelectorAll(`#set-${currentSet}`).forEach(card => {
+        card.style.display = 'flex'; // Відображаємо нові блоки
+    });
 });
