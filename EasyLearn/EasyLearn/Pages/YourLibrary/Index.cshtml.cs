81d427fb1cef3736160ef47b7988d844a429b0da
B1mba <partytopmaker@gmail.com>
2024-09-06 13:57:20 +0300

    Наново початий фронт. Створений хеадер

---
+++ EasyLearn/EasyLearn/Pages/YourLibrary/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EasyLearn.Pages.YourLibrary
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
