68dcf54b9a299c8764f05768bbbcb7b14b298eaf
MaksymKratasiuk <makshm1307@gmail.com>
2024-09-14 11:28:17 +0200

    додано таличку UserExas

---
+++ EasyLearn/EasyLearn.Models/UserExas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyLearn.Models
+{
+    internal class UserExas
+    {
+        int UserId { get; set; }
+        int UserWhoCanReadId { get; set; }
+
+        Course? Course { get; set; }
+        int? CourseId { get; set; }
+        Folder? Folder { get; set; }
+        int? FolderId { get; set; }
+        TrainingModule?TrainingModule { get; set; }
+        int? TrainingModuleId {  get; set; }
+    }
+}
